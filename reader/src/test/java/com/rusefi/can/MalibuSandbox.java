9f67d01a8c671c9cc324f8e236be2f008be99870
rusefillc <rusefillc@gmail.com>
2024-11-11 08:54:16 -0500

    malibu

--- reader/src/test/java/com/rusefi/can/MalibuSandbox.java
+++ reader/src/test/java/com/rusefi/can/MalibuSandbox.java
@@ -6,10 +6,14 @@ import java.io.IOException;
 
 public class MalibuSandbox {
     public static void main(String[] args) throws IOException {
-//        String inputFolderName = "C:\\stuff\\gen5-private\\2013-malibu\\can-traces";
-        String inputFolderName = "C:\\stuff\\gen5-private\\2013-malibu\\can-traces\\diag";
+        String inputFolderName = "C:\\stuff\\gen5-private\\2013-malibu\\can-traces";
+//        String inputFolderName = "C:\\stuff\\gen5-private\\2013-malibu\\can-traces\\diag";
+//        String inputFolderName = "C:\\stuff\\gen5-private\\2013-malibu\\can-traces\\tcu";
+//        String inputFolderName = "C:\\stuff\\gen5-private\\2013-malibu\\can-traces\\bus3";
+//        String inputFolderName = "C:\\stuff\\gen5-private\\2013-malibu\\can-traces\\wip";
 
         Launcher.dbcFileName = "C:\\stuff\\gen5-private\\2013-malibu\\malibu.dbc";
+        //Launcher.dbcFileName = "C:\\stuff\\gen5-private\\2013-malibu\\malibu3.dbc";
         DbcFile.applyOrderForStartOffset = true;
 
 //        DbcFile dbc = DbcFile.readFromFile(Launcher.dbcFileName);
